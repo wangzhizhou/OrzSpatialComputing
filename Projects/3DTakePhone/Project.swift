966fff1d900d476ffb6042cf9ad312e77834f673
wangzhizhou <wangzhizhou@bytedance.com>
2024-06-03 15:25:20 +0800

    tuist generate project

--- Projects/3DTakePhone/Project.swift
+++ Projects/3DTakePhone/Project.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let project = Project(
     name: "3DTakePhone",
     settings: .settings(configurations: [
-        .debug(name: "Debug", xcconfig: "Capture/Debug.xcconfig")
+        .debug(name: "Debug", xcconfig: "Capture/Configurations/Debug.xcconfig")
     ]),
     targets: [
         .target(
